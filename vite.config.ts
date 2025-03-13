602a3710ccadc481cb5af83172d66802399fed46
Andres Pluska <pluskaandres@gmail.com>
2025-03-13 18:51:49 -0500

    Build env fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/emergency-planner/', // This should match your repository name
+  define: {
+    'process.env': {
+      GOOGLE_AI_API_KEY: JSON.stringify(process.env.VITE_OPENAI_API_KEY),
+    }
+  }
 })
