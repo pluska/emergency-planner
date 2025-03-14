dad82043f5247cb9938c3beb944469332d64760d
Andres Pluska <pluskaandres@gmail.com>
2025-03-14 14:39:47 -0500

    --no-edit

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd())
-  return {
-    plugins: [react()],
-    base: '/emergency-planner/', // This should match your repository name
-    define: {
-      'process.env': {
-        GOOGLE_AI_API_KEY: JSON.stringify(env.VITE_OPENAI_API_KEY),
-      }
-    }
+export default defineConfig({
+  plugins: [react()],
+  base: '/emergency-planner/', // This should match your repository name
+  define: {
+    'import.meta.env.VITE_OPENAI_API_KEY': JSON.stringify(process.env.VITE_OPENAI_API_KEY)
   }
 })
